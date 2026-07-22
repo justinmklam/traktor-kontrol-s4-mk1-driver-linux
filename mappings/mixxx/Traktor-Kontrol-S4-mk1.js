c86b539ab8b583b350883ba2c3afaf1f3ebda303
Justin Lam <justinmklam@gmail.com>
2026-07-22 08:40:45 -0700

    feat: reenable vu meter

--- mappings/mixxx/Traktor-Kontrol-S4-mk1.js
+++ mappings/mixxx/Traktor-Kontrol-S4-mk1.js
@@ -38,11 +38,10 @@ TraktorKontrolS4mk1.init = function(id, debugging) {
             midi.sendShortMsg(0xB0 + chan, i, 0x00);
         }
     }*/
-    // Disabled to avoid continuous MIDI feedback into synchronous ALSA LED writes.
-    // engine.makeConnection("[Channel1]", 'VuMeter', TraktorKontrolS4mk1.vuCallback);
-    // engine.makeConnection("[Channel2]", 'VuMeter', TraktorKontrolS4mk1.vuCallback2);
-    // engine.makeConnection("[Channel3]", 'VuMeter', TraktorKontrolS4mk1.vuCallback3);
-    // engine.makeConnection("[Channel4]", 'VuMeter', TraktorKontrolS4mk1.vuCallback4);
+    engine.makeConnection("[Channel1]", 'VuMeter', TraktorKontrolS4mk1.vuCallback);
+    engine.makeConnection("[Channel2]", 'VuMeter', TraktorKontrolS4mk1.vuCallback2);
+    engine.makeConnection("[Channel3]", 'VuMeter', TraktorKontrolS4mk1.vuCallback3);
+    engine.makeConnection("[Channel4]", 'VuMeter', TraktorKontrolS4mk1.vuCallback4);
 
 
     // Set gain knobs to center value
